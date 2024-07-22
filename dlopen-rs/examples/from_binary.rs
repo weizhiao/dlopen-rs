54febeeaa1842e59b5eb799aaf4d9ea709456318
weizhiao <1207410841@qq.com>
2024-07-22 21:29:45 +0800

    test:增加从内存中加载dso的example

---
+++ dlopen-rs/examples/from_binary.rs
@@ -0,0 +1,29 @@
+use dlopen_rs::ELFLibrary;
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("/home/wei/dlopen-rs/target/release/libexample.so");
+
+    let bytes = std::fs::read(path).unwrap();
+
+    let libc = ELFLibrary::load_self("libc").unwrap();
+    let libgcc = ELFLibrary::load_self("libgcc").unwrap();
+
+    let libexample = ELFLibrary::from_binary(&bytes)
+        .unwrap()
+        .relocate(&[libgcc, libc])
+        .unwrap();
+
+    let f: dlopen_rs::Symbol<extern "C" fn(i32) -> i32> = libexample.get("c_fun_add_two").unwrap();
+    println!("{}", f(2));
+
+    let g: dlopen_rs::Symbol<extern "C" fn()> =
+        libexample.get("c_fun_print_something_else").unwrap();
+    g();
+
+    let f: dlopen_rs::Symbol<extern "C" fn()> = libexample.get("c_func_thread_local").unwrap();
+    f();
+
+    let f: dlopen_rs::Symbol<extern "C" fn()> = libexample.get("c_func_panic").unwrap();
+    f();
+}
